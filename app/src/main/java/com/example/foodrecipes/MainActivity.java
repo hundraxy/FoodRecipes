67358f0cf87119b040c9ab030fa20fd0b0a14437
Too Chhoy <131895989+hundraxy@users.noreply.github.com>
2023-10-28 20:01:27 +0700

    implement

--- app/src/main/java/com/example/foodrecipes/MainActivity.java
+++ app/src/main/java/com/example/foodrecipes/MainActivity.java
@@ -1,23 +1,33 @@
 package com.example.foodrecipes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.foodrecipes.Adapters.RandomRecipeAdapter;
 import com.example.foodrecipes.Listeners.RandomRecipeResponseListener;
 import com.example.foodrecipes.Models.RandomRecipeApiResponse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     ProgressDialog dialog;
     RequestManager manager;
     RandomRecipeAdapter randomRecipeAdapter;
     RecyclerView recyclerView;
-
+    SearchView searchView;
+    Spinner spinner;
+    List<String> tags = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +35,35 @@ public class MainActivity extends AppCompatActivity {
         dialog = new ProgressDialog(this);
         dialog.setTitle("Loading...");
 
+        searchView =findViewById(R.id.searchView_home);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                tags.clear();
+                tags.add(query);
+                manager.getRandomRecipes(randomRecipeResponseListener, tags);
+                dialog.show();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        spinner =findViewById(R.id.spinner_tags);
+        ArrayAdapter arrayAdapter =ArrayAdapter.createFromResource(
+                this,
+                R.array.tags,
+                R.layout.spinner_text
+        );
+        arrayAdapter.setDropDownViewResource(R.layout.spinner_inner_text);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(spinnerSelectedListener);
         manager =new RequestManager(this);
-        manager.getRandomRecipes(randomRecipeResponseListener);
+
+        manager =new RequestManager(this);
+        manager.getRandomRecipes(randomRecipeResponseListener, tags);
 
     }
     private final RandomRecipeResponseListener randomRecipeResponseListener = new RandomRecipeResponseListener() {
@@ -39,9 +76,37 @@ public class MainActivity extends AppCompatActivity {
             randomRecipeAdapter = new RandomRecipeAdapter(MainActivity.this, response.recipes);
             recyclerView.setAdapter(randomRecipeAdapter);
         }
+        private  final AdapterView.OnItemSelectedListener spinnerSelectedListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                tags.clear();
+                tags.add(adapterView.getSelectedItem().toString());
+                manager.getRandomRecipes(randomRecipeResponseListener,tags);
+                dialog.show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        };
         @Override
         public void didError(String message) {
             Toast.makeText(MainActivity.this, message,Toast.LENGTH_SHORT).show();
+        }
+    };
+    private  final AdapterView.OnItemSelectedListener spinnerSelectedListener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+            tags.clear();
+            tags.add(adapterView.getSelectedItem().toString());
+            manager.getRandomRecipes(randomRecipeResponseListener,tags);
+            dialog.show();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
         }
     };
 }

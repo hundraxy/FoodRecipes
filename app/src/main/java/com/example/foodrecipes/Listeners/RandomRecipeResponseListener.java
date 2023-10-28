b92feac0b5d91c0f0e228b36f895585c2e976788
Too Chhoy <131895989+hundraxy@users.noreply.github.com>
2023-10-28 19:57:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/foodrecipes/Listeners/RandomRecipeResponseListener.java
@@ -0,0 +1,9 @@
+package com.example.foodrecipes.Listeners;
+
+import com.example.foodrecipes.Models.RandomRecipeApiResponse;
+
+
+public interface RandomRecipeResponseListener {
+    void didFetch(RandomRecipeApiResponse response, String message);
+    void didError(String message);
+}
---
+++ app/src/main/java/com/example/foodrecipes/Models/AnalyzedInstruction.java
@@ -0,0 +1,5 @@
+package com.example.foodrecipes.Models;
+
+public class AnalyzedInstruction {
+
+}
